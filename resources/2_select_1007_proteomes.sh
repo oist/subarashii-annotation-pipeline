7fa606e92317df9379fab122fc9b7b86f25c5f2e
Lenard Szantho <lenard@drenal.eu>
2025-06-04 16:04:09 +0900

    updated download ID for  Turicibacter sanguinis

--- resources/2_select_1007_proteomes.sh
+++ resources/2_select_1007_proteomes.sh
@@ -29,3 +29,22 @@ for f in `cat ../list_of_genomes_accession.txt`; do
 done
 
 cd ..
+
+
+echo "GCA_013046825.1 was published 2019 and is a draft genome of Turicibacter sanguinis, strain: DSM 14220"
+echo "Maki et al 2020 sequences the same strain (albeit with different name: MOL361) and published a genome assembly https://pmc.ncbi.nlm.nih.gov/articles/PMC7303409/"
+echo "with the accession ID: GCA_013046825.1"
+echo "Let's use this insead of the missing genome GCA_004338625.1"
+
+cd proteomes1007
+i="GCA_013046825.1"
+wget -q -O "${i}.zip" "https://api.ncbi.nlm.nih.gov/datasets/v2/genome/accession/${i}/download?include_annotation_type=GENOME_FASTA&include_annotation_type=GENOME_GFF&include_annotation_type=RNA_FASTA&include_annotation_type=CDS_FASTA&include_annotation_type=PROT_FASTA&include_annotation_type=SEQUENCE_REPORT&hydrated=FULLY_HYDRATED"
+unzip -qq ${i}.zip
+if [ -f ncbi_dataset/data/${i}/protein.faa ]; then
+	cp ncbi_dataset/data/${i}/protein.faa ./${i}_protein.faa
+else
+	echo "no aa sequence available for ${i}"
+fi
+rm -rf ncbi_dataset
+rm README.md
+rm md5sum.txt
