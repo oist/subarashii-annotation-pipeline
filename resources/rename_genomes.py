1fbd26f5182e06cfe96ce112fe741064a0924404
Lenard Szantho <lenard@drenal.eu>
2025-06-06 17:05:14 +0900

    consider duplicated representatives of same species

--- resources/rename_genomes.py
+++ resources/rename_genomes.py
@@ -7,6 +7,7 @@ Authors:
 
 Version:
     - v0.1 (2025-06-06)
+    - v0.2 (2025-06-06) prepare script for duplicate species
 """
 import argparse
 import csv
@@ -19,6 +20,8 @@ def main():
     args = parser.parse_args()
 
     shortcodes = {}
+    taxa2shortcode = {}
+    samespecies = {}
     
     genomes = []
     genome_shortcodes = []
@@ -28,17 +31,30 @@ def main():
         g2t = csv.reader(inputfh)
         for row in g2t:
             genomes.append(row[0])
-            shortcode = re.sub(r'fobiota|idibacterota|laeota|bacteria|bacteriota|bacterota|mirabilota|glomotagistota|gistota|glomota|trichota|mirabilota|ficota|trophota|caulota|trophota|phobota|microbiota|mycetota|monadota|dentota|spirota|genetota|chaetota|togota|flexota|oidota|coccota|somatota|vibrionota|cutes|ota', '', row[1].split(";")[1].replace("p__",""))
-            shortcode = shortcode.replace("_","").replace("-","")
-            if len(shortcode) > 6:
-                shortcode = "{}{}".format(shortcode[:3],shortcode[-3:])
 
-            if re.match("\d", shortcode[-1]):
-                shortcode = shortcode[:-1] + 'n'
-            shortcodes.setdefault(shortcode, 0)
-            shortcodes[shortcode]+=1
+            if row[1] in taxa2shortcode:
+                print("We saw this taxa already {}".format(row[1]))
+                if row[1] not in samespecies:
+                    # find the first representative
+                    i = taxa.index(row[1])
+                    genome_shortcodes[i] = "{}s1".format(genome_shortcodes[i])
+                samespecies.setdefault(row[1], 1)
+                samespecies[row[1]] += 1
+                genome_shortcodes.append("{}s{}".format(taxa2shortcode[row[1]], samespecies[row[1]]))
+            else:
+                shortcode = re.sub(r'fobiota|idibacterota|laeota|bacteria|bacteriota|bacterota|mirabilota|glomotagistota|gistota|glomota|trichota|mirabilota|ficota|trophota|caulota|trophota|phobota|microbiota|mycetota|monadota|dentota|spirota|genetota|chaetota|togota|flexota|oidota|coccota|somatota|vibrionota|cutes|ota', '', row[1].split(";")[1].replace("p__",""))
+                shortcode = shortcode.replace("_","").replace("-","")
+                if len(shortcode) > 6:
+                    shortcode = "{}{}".format(shortcode[:3],shortcode[-3:])
 
-            genome_shortcodes.append("{}{}".format(shortcode, shortcodes[shortcode]))
+                if re.match("\d", shortcode[-1]):
+                    shortcode = shortcode[:-1] + 'n'
+                shortcodes.setdefault(shortcode, 0)
+                shortcodes[shortcode]+=1
+
+                genome_shortcodes.append("{}{}".format(shortcode, shortcodes[shortcode]))
+                taxa2shortcode[row[1]] = genome_shortcodes[-1]
+
             taxa.append(row[1])
 
     with open(args.outfile, "w") as outputfh:
