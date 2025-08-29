b280d1998e106ea001af8cb70b2513335d7536bd
Lenard Szantho <lenard@drenal.eu>
2025-08-29 11:30:24 +0900

    fixing eggnog error, removing unncessary config lines

--- snakemake/1_cluster/scripts/select_proteomes.sh
+++ snakemake/1_cluster/scripts/select_proteomes.sh
@@ -25,20 +25,24 @@ for f in `cat ${IDS}`; do
 			cp ${BASEDIR}/archaea/${f}_protein.faa.gz ${OUTDIR}/
 			gzip -d ${OUTDIR}/${f}_protein.faa.gz
 		else
-			i=`echo $f | sed 's/GB_//' | sed 's/RS_//'`
-			echo "$f not found, so downloading from NCBI using ID: $i"
-			wget -q -O "${i}.zip" "https://api.ncbi.nlm.nih.gov/datasets/v2/genome/accession/${i}/download?include_annotation_type=GENOME_FASTA&include_annotation_type=GENOME_GFF&include_annotation_type=RNA_FASTA&include_annotation_type=CDS_FASTA&include_annotation_type=PROT_FASTA&include_annotation_type=SEQUENCE_REPORT&hydrated=FULLY_HYDRATED"
-			mkdir ziptemp; cd ziptemp
-			unzip -qq ../${i}.zip
-			cd ..
-			if [ -f ziptemp/ncbi_dataset/data/${i}/protein.faa ]; then
-				cp ziptemp/ncbi_dataset/data/${i}/protein.faa ${OUTDIR}/${i}_protein.faa
-			else
-				echo "no aa sequence available for ${i}, copying the na sequence"
-				cp ziptemp/ncbi_dataset/data/${i}/${i}*_genomic.fna ${OUTDIR}/${i}_genomic.fna
-			fi
-			rm -rf ziptemp
-			rm ${i}.zip
+			echo "$f not found, please specify a valid accession ID"
+			exit -1
+
+			# the code below could download missing genomes from NCBI if you're sure the accession ID is valid
+			#i=`echo $f | sed 's/GB_//' | sed 's/RS_//'`
+			#echo "$f not found, so downloading from NCBI using ID: $i"
+			#wget -q -O "${i}.zip" "https://api.ncbi.nlm.nih.gov/datasets/v2/genome/accession/${i}/download?include_annotation_type=GENOME_FASTA&include_annotation_type=GENOME_GFF&include_annotation_type=RNA_FASTA&include_annotation_type=CDS_FASTA&include_annotation_type=PROT_FASTA&include_annotation_type=SEQUENCE_REPORT&hydrated=FULLY_HYDRATED"
+			#mkdir ziptemp; cd ziptemp
+			#unzip -qq ../${i}.zip
+			#cd ..
+			#if [ -f ziptemp/ncbi_dataset/data/${i}/protein.faa ]; then
+			#	cp ziptemp/ncbi_dataset/data/${i}/protein.faa ${OUTDIR}/${i}_protein.faa
+			#else
+			#	echo "no aa sequence available for ${i}, copying the na sequence"
+			#	cp ziptemp/ncbi_dataset/data/${i}/${i}*_genomic.fna ${OUTDIR}/${i}_genomic.fna
+			#fi
+			#rm -rf ziptemp
+			#rm ${i}.zip
 		fi
 	fi
 done
