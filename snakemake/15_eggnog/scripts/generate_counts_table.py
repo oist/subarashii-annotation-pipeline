6ea6deb910717f1b3efb15f67bbf5eafd4b79b42
Lenard Szantho <lenard@drenal.eu>
2026-05-14 12:36:08 +0900

    updated eggnog counts table generation

--- snakemake/15_eggnog/scripts/generate_counts_table.py
+++ snakemake/15_eggnog/scripts/generate_counts_table.py
@@ -15,6 +15,12 @@ Authors:
     - Lenard Szantho <lenard@drenal.eu>
 
 Version:
+    - v0.7 (2026-03-19)
+        - feather export out of memory for 150k x 207k matrix, trying to make more memory-efficient
+    - v0.6 (2026-03-16)
+        - feather export restored and made into an option
+    - v0.5 (2026-03-15)
+        - bugfix: one extra comma at the end of lines removed
     - v0.4 (2026-02-26)
         - generalizing for KEGG KO and EC category columns as well
     - v0.3 (2026-02-13)
@@ -32,10 +38,7 @@ import csv
 import statistics
 from pathlib import Path
 from time import perf_counter
-#import pyarrow.feather as feather
-#import pandas as pd
-
-
+from memory_profiler import profile
 
 def main():
     t1 = perf_counter()
@@ -47,6 +50,8 @@ def main():
     parser.add_argument("--map-col-eggnog", help="Column name in the mapping csv file corresponding to the genome column in the eggnog annotation file.")
     parser.add_argument("-a", "--all", help="Output all OGs, not just COGs. Has effect only if '-c' is set to 'cog'", action="store_true", default=False)
     parser.add_argument("-c", "--column", help="Column to count. By default the 'cog', i.e. counting the eggnog_OG column.", default="cog", choices=['cog', 'ko', 'ec'])
+    parser.add_argument("-f", "--feather", help="Output as feather format as well", action="store_true", default=False)
+    parser.add_argument("-r", "--reduce", help="Reduce the genomes to a list of accession IDs or genome IDs (supply a file with a list separated by newline chars)")
     args = parser.parse_args()
 
     columns_mapping = {
@@ -70,6 +75,9 @@ def main():
     # stores genes associated to a given COG
     # { "COG0043": ["gene1", "gene2", "gene3", ...] }
     cog2genes = dict()
+    # genomes associated with a cog
+    cog2nrgenomes = dict()
+
 
     print(f"Processing EggNog annotation file {args.eggnog} ...")
     with open(args.eggnog) as csvfile:
@@ -98,6 +106,10 @@ def main():
             for c in cog_list:
                 genome2cogcounts[genome].setdefault(c, 0)
                 genome2cogcounts[genome][c] += 1
+
+                cog2nrgenomes.setdefault(c,0)
+                cog2nrgenomes[c] += 1
+
                 cogs.add(c)
 
                 cog2genes.setdefault(c, [])
@@ -107,7 +119,7 @@ def main():
     # change names if mapping is provided
     if args.map:
         print(f"Loading mapping file {args.map} ...")
-        # { "genomeID" : {"accession" : "RS_GCF_01XXXXX", "taxa": "d__Bacteria;p__;c__;p__;f__;g__;s__"
+        # { "genomeID" : {"accession" : "RS_GCF_01XXXXX", "taxa": "d__Bacteria;p__;c__;p__;f__;g__;s__" }, "genomeID2": {}, ... }
         mapping = {}
         mapping_columns = []
         index_col_id = 0
@@ -131,6 +143,38 @@ def main():
                 mapping[row[index_col_id]]["accession"] = row[accession_col_id]
                 mapping[row[index_col_id]]["taxa"] = row[taxa_col_id]
 
+    if args.map and args.reduce:
+        accessions_to_keep = []
+        with open(args.reduce, "r") as inputfh:
+            for l in inputfh:
+                if len(l.strip()) > 0:
+                    accessions_to_keep.append(l.strip())
+        print(f"{len(accessions_to_keep)} number of accessions should be retained.")
+        genome_ids_to_delete = []
+        for genome in genome2cogcounts:
+            if mapping[genome]["accession"] not in accessions_to_keep:
+                genome_ids_to_delete.append(genome)
+        print(f"{len(genome_ids_to_delete)} genomes will be removed from the count table")
+        for g in genome_ids_to_delete:
+            if g in genome2cogcounts:
+                del genome2cogcounts[g]
+
+    # check how often we have singleton COGs
+    cog_to_delete = []
+    for cog in cog2nrgenomes:
+        if cog2nrgenomes[cog] == 1:
+            #print(f"{cog} has only one genome in it")
+            cog_to_delete.append(cog)
+
+    print(f"{len(cog_to_delete)} categories are singleton, to be deleted")
+
+    for cog in cog_to_delete:
+        for genome in genome2cogcounts:
+            if cog in genome2cogcounts[genome]:
+                del genome2cogcounts[genome][cog]
+        cogs.discard(cog)
+        
+
     print(f"Writing count table to {args.output} ...")
     index_label = "genome"
 
@@ -142,19 +186,33 @@ def main():
         outputfh.write(",".join(sorted(cogs)))
         outputfh.write("\n")
         for genome in genome2cogcounts:
-            outputfh.write(f"{genome},")
+            outputfh.write(f"{genome}")
             if args.map:
-                outputfh.write(f"{mapping[genome]['accession']},{mapping[genome]['taxa']},")
+                outputfh.write(f",{mapping[genome]['accession']},{mapping[genome]['taxa']}")
             for cog in sorted(cogs):
                 if cog in genome2cogcounts[genome]:
-                    outputfh.write(f"{genome2cogcounts[genome][cog]},")
+                   outputfh.write(f",{genome2cogcounts[genome][cog]}")
                 else:
-                    outputfh.write(",")
+                    outputfh.write(f",")
             outputfh.write("\n")
 
     with open(f"{args.output}_{args.column}2genes.list", "w") as outputfh:
         for cog in sorted(cog2genes.keys()):
             outputfh.write("{}\t{}\n".format(cog, ",".join(cog2genes[cog])))
+
+    if args.feather:
+        import pyarrow.feather as feather
+        import pandas as pd
+        import numpy as np
+
+        # trying to use intc to reduce space, but apparently pandas just ignores it and uses doubles
+        df = pd.DataFrame(columns=sorted(cogs), index=list(genome2cogcounts.keys()), dtype=np.intc)
+        for genome in genome2cogcounts:
+            df.loc[genome] = pd.Series(genome2cogcounts[genome])
+        # this duplicates the DataFrame in the memory, that would lead to OOF-kill for large datasets
+        #df = df.fillna(0)
+
+        feather.write_feather(df, f"{args.output}_genome2{args.column}counts.feather")
 
 
 if __name__ == "__main__":
