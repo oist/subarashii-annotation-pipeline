dac3f7ec21c0edd92ffcc68c25462a047ac6deb8
Lenard Szantho <lenard@drenal.eu>
2026-05-14 15:20:07 +0900

    updating the cog cluster creator script

--- snakemake/1_cluster/scripts/create_cog_clusters.py
+++ snakemake/1_cluster/scripts/create_cog_clusters.py
@@ -2,14 +2,17 @@
 """
 Create COG clusters based on EggNog annotation file
 
-Also writes two logs in the same directory:
+Also writes four reports in the same directory:
    cog2genes.tsv
    genes2cog.tsv
+   family_type_counts.tsv -- Singleton, Duplets, Triplets and Huge counts
+   family_details.tsv -- number of sequences and sequence length stats and if it has paralogs or not
 
 Authors:
     - Lenard Szantho <lenard@drenal.eu>
 
 Version:
+    - v0.2 (2026-05-14): handling multiple root-level COGs
     - v0.1 (2025-10-07)
 """
 
@@ -34,7 +37,7 @@ def main():
     parser.add_argument("-o", "--output", help="Output base directory.", required=True)
     parser.add_argument("-f", "--fasta", help="Directory with the per genome fasta files", required=True)
     parser.add_argument("-e", "--eggnog", help="Path to EggNog's annotation file", required=True)
-    parser.add_argument("-c", "--cutoff", help="Cutoff number for huge families", default=300, type=float)
+    parser.add_argument("-c", "--cutoff", help="Cutoff number for huge families. By default: 5 times the number of genomes.", type=int)
     args = parser.parse_args()
 
     out_root = Path(args.output)
@@ -52,25 +55,40 @@ def main():
                 continue
             gene = rows[0].strip()
             genome = gene.split("_g")[0]
-            cog = rows[4].split("@")[0].strip()
+
+            cog_list = []
+            # e.g.: COG0457@1|root,COG0823@1|root,COG2885@1|root,COG0457@2|Bacteria,COG0823@2|Bacteria
+            for c in rows[4].split(","):
+                if "|root" in c:
+                    cog_list.append(c.split("@")[0].strip()
 
             genome2genes2cog.setdefault(genome, {})
-            genome2genes2cog[genome][gene] = cog
-            cog2genes.setdefault(cog, []).append(gene)
+            genome2genes2cog[genome].setdefault(gene, [])
+            for c in cog_list:
+                genome2genes2cog[genome][gene].append(c)
+                cog2genes.setdefault(c, [])
+                cog2genes[c].append(gene)
 
     print(f"Sorting {len(genome2genes2cog)} genomes into COG files under {out_root} ...")
     for genome in tqdm(genome2genes2cog):
         genome_file = {rec.id: rec for rec in SeqIO.parse(f"{args.fasta}/{genome}.faa", "fasta")}
         for gene in genome2genes2cog[genome]:
-            cog2seqlengths.setdefault(genome2genes2cog[genome][gene], []).append(len(genome_file[gene].seq))
-            with open(out_root / f"{genome2genes2cog[genome][gene]}.faa", "a") as outputfh:
-                outputfh.write(f">{gene}\n{genome_file[gene].seq}\n")
+            for c in genome2genes2cog[genome][gene]:
+                cog2seqlengths.setdefault(c, []).append(len(genome_file[gene].seq))
+                with open(out_root / f"{c}.faa", "a") as outputfh:
+                    outputfh.write(f">{gene}\n{genome_file[gene].seq}\n")
 
     print("Order families into subdirectories based on the number of genomes")
     type_counter = dict()
     for t in ("Singleton", "Duplets", "Triplets", "Normal", "Huge"):
         ( out_root / t ).mkdir(exist_ok=True)
         type_counter[t] = 0
+
+    cutoff_size = 300
+    if not args.cutoff:
+        cutoff_size = 5 * len(genome2genes2cog)
+    else:
+        cutoff_size = args.cutoff
 
     for cog in cog2genes:
         size = len(cog2genes[cog])
@@ -83,7 +101,7 @@ def main():
         elif size == 3:
             type_counter["Triplets"] += 1
             ( out_root / 'Triplets' / f"{cog}.faa" ).symlink_to(f"../{cog}.faa")
-        elif size >= args.cutoff:
+        elif size >= cutoff_size:
             type_counter["Huge"] += 1
             ( out_root / 'Huge' / f"{cog}.faa" ).symlink_to(f"../{cog}.faa")
         else:
@@ -94,7 +112,8 @@ def main():
     with open(out_root / 'genes2cog.tsv', 'w') as outputfh:
         for genome in genome2genes2cog:
             for gene in genome2genes2cog[genome]:
-                outputfh.write(f"{gene}\t{genome2genes2cog[genome][gene]}\n")
+                cogs = ";".join(genome2genes2cog[genome][gene])
+                outputfh.write(f"{gene}\t{cogs}\n")
 
     with open(out_root / 'cog2genes.tsv', 'w') as outputfh:
         for cog in cog2genes:
@@ -102,7 +121,8 @@ def main():
             outputfh.write(",".join(cog2genes[cog]))
             outputfh.write("\n")
     
-    with open(out_root / 'family_type_counts.tsv', 'w') as outputfh:       
+    with open(out_root / 'family_type_counts.tsv', 'w') as outputfh:
+        #outputfh.write("Name\tThreshold\tSize\n")
         for t in ("Singleton", "Duplets", "Triplets", "Normal", "Huge"):
             outputfh.write(f"{t}\t{type_counter[t]}\n")
 
